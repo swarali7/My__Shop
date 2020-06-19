b64ef7eaaaa90396bd201833db7c54585c5586f3
Swarali Satish Kestikar <swarali1999@gmail.com>
2020-06-19 20:50:54 +0530

    Added Entity Framework

--- My__Shop/My__Shop.Core/Models/BaseEntity.cs
+++ My__Shop/My__Shop.Core/Models/BaseEntity.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace My__Shop.Core.Models
 {
-    public abstract class BaseEntity
+    public abstract class BaseEntity 
     {
         public string Id { get; set; }
-        public DateTimeOffset CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
         public BaseEntity()
         {
             this.Id = Guid.NewGuid().ToString();
---
+++ My__Shop/My__Shop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using My__Shop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My__Shop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            :base("DefaultConnection") {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        
+    }
+}
