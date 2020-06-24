549f4f416a59439f5e2aef3bd23d1dcdb68136f9
Swarali Satish Kestikar <swarali1999@gmail.com>
2020-06-24 20:41:50 +0530

    Basket Service Part-1 Implemented

---
+++ My__Shop/My__Shop.Services/BasketService.cs
@@ -0,0 +1,110 @@
+﻿using My__Shop.Core.Contracts;
+using My__Shop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace My__Shop.Services
+{
+    public class BasketService
+    {
+        IRepository<Product> productContext;
+        IRepository<Basket> basketContext;
+
+        public const string BasketSessionName = "eCommerceBasket";
+
+        public BasketService(IRepository<Product> ProductContext, IRepository<Basket> BasketContext)
+        {
+            this.basketContext = BasketContext;
+            this.productContext = ProductContext;
+        }
+
+        private Basket GetBasket(HttpContextBase httpContext, bool CreateIfNull)
+        {
+            HttpCookie cookie = httpContext.Request.Cookies.Get(BasketSessionName);
+
+            Basket basket = new Basket();
+
+            if(cookie != null)
+            {
+                string basketId = cookie.Value;
+                if(!string.IsNullOrEmpty(basketId))
+                {
+                    basket = basketContext.Find(basketId);
+                }
+                else
+                {
+                    if(CreateIfNull)
+                    {
+                        basket = CreateNewBasket(httpContext);
+                    }
+                }
+            }
+            else
+            {
+                if (CreateIfNull)
+                {
+                    basket = CreateNewBasket(httpContext);
+                }
+            }
+
+            return basket;
+
+        }
+
+        private Basket CreateNewBasket(HttpContextBase httpContext)
+        {
+            Basket basket = new Basket();
+            basketContext.Insert(basket);
+            basketContext.Commit();
+
+            HttpCookie cookie = new HttpCookie(BasketSessionName);
+            cookie.Value = basket.Id;
+            cookie.Expires = DateTime.Now.AddDays(1);
+            httpContext.Response.Cookies.Add(cookie);
+
+            return basket;
+        }
+
+        public void AddToBasket(HttpContextBase httpContext, string productId)
+        {
+            Basket basket = GetBasket(httpContext true);
+            BasketItem item = basket.BasketItems.FirstOrDefault(i => i.ProductId == productId);
+
+            if(item==null)
+            {
+                item = new BasketItem()
+                {
+                    BasketId = basket.Id,
+                    ProductId = productId,
+                    Quantity = 1
+                };
+
+                basket.BasketItems.Add(item);
+            }
+            else
+            {
+                item.Quantity = item.Quantity;
+            }
+
+            basketContext.Commit();
+        }
+        
+        public void RemoveFromBasket(HttpContextBase httpContext, string itemId)
+        {
+            Basket basket = GetBasket(httpContext, true);
+            BasketItem item = basket.BasketItems.FirstOrDefault(i => i.Id == itemId);
+
+            if(item != null)
+            {
+                basket.BasketItems.Remove(item);
+                basketContext.Commit();
+            }
+           
+        }
+        
+    }
+}
